f5ed95352a7ed4306cd5ce6e34021c0ec9322234
Ap274 <aldopiresaps@gmail.com>
2020-11-28 12:53:29 -0300

    test(exercicio-08): adding unit tests and remdove loggers

--- exercicios/src/exercicio-08/index.js
+++ exercicios/src/exercicio-08/index.js
@@ -1,35 +1,24 @@
-const logger = require('../../logger');
-
-const stringPont = '10, 20, 20, 8, 25, 3, 0, 30, 1';
-
-function avaliaPont(pont) {
-  const listaPontos = pont.split(', ');
+const avaliaPontuacoes = (pontuacoes) => {
+  const listaPontos = [];
+  pontuacoes.split(',').map((item) => {
+    listaPontos.push(Number(item));
+    return item;
+  });
   let qtdQuebraRecordes = 0;
   let piorJogo = 1;
-  // logger.info(listaPontos)
-  let maiorPont = listaPontos[0];
-  let menorPont = listaPontos[0];
+  let maiorPontuacao = listaPontos[0];
+  let menorPontuacao = listaPontos[0];
 
-  for (let i = 1; i <= listaPontos.lenght; i++) {
-    if (listaPontos[i] > maiorPont) {
-      maiorPont = listaPontos[i];
+  for (let i = 1; i <= listaPontos.length; i++) {
+    if (listaPontos[i] > maiorPontuacao) {
+      maiorPontuacao = listaPontos[i];
       qtdQuebraRecordes++;
-    } else if (listaPontos[i] < menorPont) {
-      menorPont = listaPontos[i];
+    } else if (listaPontos[i] < menorPontuacao) {
+      menorPontuacao = listaPontos[i];
       piorJogo = i + 1;
     }
-    logger.info(`maior pontuação = ${maiorPont}`);
-    logger.info(`menor pontuação = ${menorPont}`);
   }
   return [qtdQuebraRecordes, piorJogo];
-}
-
-const calculaPontuacoes = () => {
-  logger.info('Exercício 08');
-  logger.info('------------');
-  logger.info(avaliaPont(stringPont));
-  logger.info('------------');
 };
 
-module.exports = { calculaPontuacoes };
-/// REVER ESSA QUESTÃO**********
+module.exports = { avaliaPontuacoes };
